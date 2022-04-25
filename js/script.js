8b6900b007e53f6385255d58b02829f726f649bc
Jhoseb <jhoseb29@gmail.com>
2022-04-25 18:02:51 -0500

    poniendo proyectos

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ window.addEventListener('load', () => {
     const scan = document.querySelector('.scan')
     scan.style.opacity = 0
     scan.style.visibility = 'hidden'
-})       
+})
 
-function toggleMenu(){
+function toggleMenu() {
     var menuToggle = document.querySelector('.toggle');
     var menu = document.querySelector('.menu');
     menuToggle.classList.toggle('active')
     menu.classList.toggle('active')
-    
-    ul.addEventListener('click', (e) =>{
-        
+
+    ul.addEventListener('click', (e) => {
+
     })
 }
 
